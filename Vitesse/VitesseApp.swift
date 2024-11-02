f36013c44ed89dc092b5d20a306f245eec047604
Hamdi <TLiLiOS@yahoo.com>
2024-11-02 16:58:29 +0100

    Initial Commit

---
+++ Vitesse/VitesseApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitesseApp.swift
+//  Vitesse
+//
+//  Created by MacBook Air on 02/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct VitesseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
